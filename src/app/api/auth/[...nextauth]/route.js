853d5ca7f876a9d6365a6247ca02a4b6fc05153a
AmukthaMalyadaGaje <amukthagaje@gmail.com>
2023-11-23 14:48:52 +0530

    Final Authentication Files

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,6 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-  // Configure one or more authentication providersq
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -13,7 +12,6 @@ export const authOptions = {
       clientId: process.env.GIHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
-    // ...add more providers here
 ],
 secret : process.env.SECRET,
 }
